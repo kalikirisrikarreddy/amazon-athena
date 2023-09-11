e2fc412aa2c3ea4bb31568c11542d15c32808b3e
Srikar Reddy <kalikirisrikarreddy@users.noreply.github.com>
2023-09-11 17:27:27 +0530

    Update JsonDataProducer.java

--- src/main/java/amazon/athena/JsonDataProducer.java
+++ src/main/java/amazon/athena/JsonDataProducer.java
@@ -1,9 +1,12 @@
 package amazon.athena;
 
+import com.fasterxml.jackson.core.util.MinimalPrettyPrinter;
+import com.github.javafaker.ChuckNorris;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.YearMonth;
@@ -11,6 +14,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import java.util.zip.GZIPOutputStream;
 
 import org.apache.commons.lang3.StringUtils;
@@ -39,7 +43,8 @@ public class JsonDataProducer {
 	private static final int NO_OF_RECS_PER_FILE = Integer.parseInt(System.getenv("NO_OF_RECS_PER_FILE"));
 	private static final int FROM_YEAR = Integer.parseInt(System.getenv("FROM_YEAR"));
 	private static final int TO_YEAR = Integer.parseInt(System.getenv("TO_YEAR"));
-	private static final List<Employee> EMPLOYEES = new ArrayList<Employee>(NO_OF_RECS_PER_FILE);
+	private static final String THOUSAND_ASTERISKS = "***************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************";
+	private static final List<Employee> EMPLOYEES = new ArrayList<Employee>(5);
 	private static final String HYPHEN = "-";
 	private static final ObjectWriter OBJECT_WRITER = new ObjectMapper().writerFor(List.class);
 
@@ -57,8 +62,10 @@ public class JsonDataProducer {
 			Name name = faker.name();
 			Company company = faker.company();
 			Address address = faker.address();
+			ChuckNorris chuckNorris = faker.chuckNorris();
 			EMPLOYEES.add(new Employee(UUID.randomUUID().toString(), name.firstName() + HYPHEN,
-					name.lastName() + HYPHEN, company.name(), address.fullAddress(), address.secondaryAddress()));
+					name.lastName() + HYPHEN, company.name(), address.fullAddress(),
+					THOUSAND_ASTERISKS + chuckNorris.fact() + THOUSAND_ASTERISKS));
 		}
 		for (int year = FROM_YEAR; year <= TO_YEAR; year++) {
 			for (int month = 1; month <= 12; month++) {
@@ -74,7 +81,7 @@ public class JsonDataProducer {
 
 	private static void generateFileForADay(final int y, final int m, int day) {
 		String variableSuffix = String.valueOf(y) + String.valueOf(m) + String.valueOf(day);
-		for (int recordIndex = 0; recordIndex < NO_OF_RECS_PER_FILE; recordIndex++) {
+		for (int recordIndex = 0; recordIndex < 5; recordIndex++) {
 			Employee employee = EMPLOYEES.get(recordIndex);
 			employee.setId(UUID.randomUUID().toString());
 			employee.setFirstname(
@@ -82,16 +89,23 @@ public class JsonDataProducer {
 			employee.setLastname(StringUtils.substringBefore(employee.getLastname(), HYPHEN) + HYPHEN + variableSuffix);
 		}
 
-		File file = new File("f" + LocalDate.of(y, m, day).format(DateTimeFormatter.BASIC_ISO_DATE) + ".csv.gz");
+		File file = new File("f" + LocalDate.of(y, m, day).format(DateTimeFormatter.BASIC_ISO_DATE) + ".json.gz");
 
 		try (FileOutputStream fos = new FileOutputStream(file);
 				BufferedOutputStream bos = new BufferedOutputStream(fos);
 				GZIPOutputStream gos = new GZIPOutputStream(bos);) {
-			OBJECT_WRITER.writeValue(gos, EMPLOYEES);
+				EMPLOYEES.forEach(employee -> {
+					try {
+						gos.write(new ObjectMapper().writeValueAsBytes(employee));
+						gos.write("\n".getBytes(StandardCharsets.UTF_8));
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+				});
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-		String s3Key = "birthday=" + LocalDate.of(y, m, day).format(DateTimeFormatter.ISO_LOCAL_DATE) + "/data.csv.gz";
+		String s3Key = "birthday=" + LocalDate.of(y, m, day).format(DateTimeFormatter.ISO_LOCAL_DATE) + "/data.json.gz";
 		s3Client.putObject(PutObjectRequest.builder().bucket("athena-datasets-1").key(s3Key).build(),
 				RequestBody.fromFile(file));
 		file.delete();
